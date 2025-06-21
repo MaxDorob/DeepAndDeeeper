a969b590af535660352fe776e0e01f3eb8433d13
MaxDorob <maxdor81@gmail.com>
2025-06-22 02:40:35 +0600

    Spawn issue fix

---
+++ Source/ScatterValidator_OtherBuildings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Shashlichnik
+{
+    public class ScatterValidator_OtherBuildings : ScattererValidator
+    {
+        public ScatterValidator_OtherBuildings() : base() { }
+        public override bool Allows(IntVec3 c, Map map)
+        {
+            var building = c.GetEdifice(map);
+            if (building == null)
+            {
+                return true;
+            }
+            return building.def.destroyable;
+        }
+    }
+}

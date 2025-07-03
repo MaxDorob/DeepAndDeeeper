b7ff89e9b43783d738ddafd526bb3a56fbc080d6
Макс Дороболюк <dmv_01@mail.ru>
2025-07-03 23:12:56 +0600

    Potential fix for some reports

--- Source/GenStep_FindCaveExit.cs
+++ Source/GenStep_FindCaveExit.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
+using Verse.Noise;
 
 namespace Shashlichnik
 {
@@ -17,12 +18,32 @@ namespace Shashlichnik
                 return 2162632;
             }
         }
-
+        private IntVec3 FindCellForExit(Map map, bool shouldBeStandable)
+        {
+            IntVec3 result;
+            if (shouldBeStandable)
+            {
+                if (CellFinder.TryFindRandomCell(map, (IntVec3 cell) => cell.Standable(map) && (float)cell.DistanceToEdge(map) > ClearRadius + 1f, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    return FindCellForExit(map, false);
+                }
+            }
+            if (CellFinder.TryFindRandomCell(map, (IntVec3 cell) => cell.DistanceToEdge(map) > ClearRadius + 1f, out result) && result.InBounds(map))
+            {
+                return result;
+            }
+            Log.Warning("Can't find a valid place for a cave exit, placing at center");
+            return map.Center;
+        }
         public override void Generate(Map map, GenStepParams parms)
         {
             var shouldFindCave = Rand.Chance(0.5f);
-            CellFinder.TryFindRandomCell(map, (IntVec3 cell) => (!shouldFindCave  || cell.Standable(map)) && (float)cell.DistanceToEdge(map) > ClearRadius + 1f, out var intVec);
-            foreach (IntVec3 c in GenRadial.RadialCellsAround(intVec, ClearRadius, true))
+            var exitCell = FindCellForExit(map, shouldFindCave);
+            foreach (IntVec3 c in GenRadial.RadialCellsAround(exitCell, ClearRadius, true))
             {
                 foreach (Thing thing in from t in c.GetThingList(map).ToList<Thing>()
                                         where t.def.destroyable
@@ -31,8 +52,8 @@ namespace Shashlichnik
                     thing.Destroy(DestroyMode.Vanish);
                 }
             }
-            GenSpawn.Spawn(ThingMaker.MakeThing(DefsOf.ShashlichnikCaveExit, null), intVec, map, WipeMode.Vanish);
-            MapGenerator.PlayerStartSpot = intVec;
+            GenSpawn.Spawn(ThingMaker.MakeThing(DefsOf.ShashlichnikCaveExit, null), exitCell, map, WipeMode.Vanish);
+            MapGenerator.PlayerStartSpot = exitCell;
         }
 
         public const float ClearRadius = 4.5f;

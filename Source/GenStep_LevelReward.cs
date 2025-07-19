928794c5a071aeaf44b66684479cef44e5fb5d6a
MaxDorob <maxdor81@gmail.com>
2025-07-19 18:32:51 +0600

    Reward for reaching the level

---
+++ Source/GenStep_LevelReward.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Shashlichnik
+{
+    public class GenStep_LevelReward : GenStep
+    {
+        ThingSetMakerDef reward;
+        public override int SeedPart => 291812;
+        public override void Generate(Map map, GenStepParams parms)
+        {
+            var rewards = reward.root.Generate();
+            foreach (var thing in rewards)
+            {
+                if (CellFinder.TryFindRandomCellNear(MapGenerator.PlayerStartSpot, map, 15, c => c.Standable(map) && GenSpawn.CanSpawnAt(thing.def, c, map, canWipeEdifices: false), out var cell, 600))
+                {
+                    GenSpawn.Spawn(thing, cell, map);
+                }
+                else
+                {
+                    Log.Warning($"Can't find a place for {thing}");
+                }
+            }
+        }
+    }
+}

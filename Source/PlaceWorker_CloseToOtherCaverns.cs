d07fb06cd435f157f464559ee9311d6dc008aefd
MaxDorob <maxdor81@gmail.com>
2025-07-22 21:32:55 +0600

    NRE workaround I guess

--- Source/PlaceWorker_CloseToOtherCaverns.cs
+++ Source/PlaceWorker_CloseToOtherCaverns.cs
@@ -13,8 +13,8 @@ namespace Shashlichnik
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
         {
             if (map.listerThings.ThingsInGroup(ThingRequestGroup.MapPortal).OfType<CaveEntrance>().Any(c => c.Position.InHorDistOf(loc, CaveEntranceTracker.blockRadius)) ||
-                map.listerThings.ThingsInGroup(ThingRequestGroup.Blueprint).OfType<Blueprint_Build>().Any(x => x.BuildDef.thingClass == typeof(CaveEntrance) && x.Position.InHorDistOf(loc, CaveEntranceTracker.blockRadius)) ||
-                map.listerThings.ThingsInGroup(ThingRequestGroup.BuildingFrame).OfType<Frame>().Any(x=>x.BuildDef.thingClass == typeof(CaveEntrance) && x.Position.InHorDistOf(loc, CaveEntranceTracker.blockRadius)))
+                map.listerThings.ThingsInGroup(ThingRequestGroup.Blueprint).OfType<Blueprint_Build>().Any(x => x.BuildDef?.thingClass == typeof(CaveEntrance) && x.Position.InHorDistOf(loc, CaveEntranceTracker.blockRadius)) ||
+                map.listerThings.ThingsInGroup(ThingRequestGroup.BuildingFrame).OfType<Frame>().Any(x => x.BuildDef?.thingClass == typeof(CaveEntrance) && x.Position.InHorDistOf(loc, CaveEntranceTracker.blockRadius)))
                 return new AcceptanceReport("ShashlichnikCloseToOtherCavern".Translate());
             return true;
         }

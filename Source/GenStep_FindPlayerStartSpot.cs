0e41b6384bfa13c31fcd0817fd7fdb9fb04e3013
Макс Дороболюк <dmv_01@mail.ru>
2026-04-20 05:22:32 +0600

    Pawns spawns in the caverns

---
+++ Source/GenStep_FindPlayerStartSpot.cs
@@ -0,0 +1,75 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Shashlichnik
+{
+    public class GenStep_FindPlayerStartSpot : RimWorld.GenStep_FindPlayerStartSpot
+    {
+        public override int SeedPart => 9276172;
+
+        public override void Generate(Map map, GenStepParams parms)
+        {
+            var exit = map.listerThings.ThingsOfDef(DefsOf.ShashlichnikCaveExit).FirstOrDefault();
+            if (exit == null)
+            {
+                Log.Error($"Cannot find an exit");
+                MapGenerator.PlayerStartSpot = IntVec3.Invalid;
+                base.Generate(map, parms);
+                return;
+            }
+            var usedRects = MapGenerator.GetOrGenerateVar<List<CellRect>>("UsedRects");
+            IntVec3 spot;
+            if (!CellFinderLoose.TryGetRandomCellWith(c => !IsInsideAnyCellRect(usedRects, c) && !map.reachability.CanReach(c, exit.InteractionCell, Verse.AI.PathEndMode.ClosestTouch, TraverseMode.ByPawn | TraverseMode.PassDoors, Danger.Deadly) && c.Standable(map),
+                map,
+                50,
+                out spot))
+            {
+                Log.Warning("Cannot find standable position");
+                if (!CellFinderLoose.TryGetRandomCellWith(c => !IsInsideAnyCellRect(usedRects, c) && !map.reachability.CanReach(c, exit.InteractionCell, Verse.AI.PathEndMode.ClosestTouch, TraverseMode.ByPawn | TraverseMode.PassDoors, Danger.Deadly),
+                    map,
+                    50,
+                    out spot))
+                {
+                    Log.Warning($"Failed to find any position without access to the exit");
+                    spot = CellFinderLoose.RandomCellWith(c => !IsInsideAnyCellRect(usedRects, c) && exit.Position.DistanceTo(c) > 30f, map);
+                }
+            }
+
+            MapGenerator.PlayerStartSpot = spot;
+
+            foreach (var cell in GenRadial.RadialCellsAround(spot, 5.5f, true))
+            {
+                if (cell.DistanceTo(spot) < 3.4f)
+                {
+                    foreach(var thing in map.thingGrid.ThingsListAt(cell).Where(t => t.def.passability != Traversability.Standable).ToList())
+                    {
+                        thing.Destroy();
+                    }
+                }
+                else
+                {
+                    var chance = 0.6f;
+                    if (cell.GetEdifice(map) != null)
+                    {
+                        chance = 0.06f;
+                    }
+                    if (Rand.Chance(chance))
+                    {
+                        RoofCollapserImmediate.DropRoofInCells(cell, map, null);
+                    }
+                }
+            }
+        }
+
+        private bool IsInsideAnyCellRect(IEnumerable<CellRect> cellRects, IntVec3 cell)
+        {
+            return cellRects.Any(r => r.Contains(cell));
+        }
+
+    }
+}
---
+++ Source/ScenPart_PlayerPawnsArriveMethod.cs
@@ -0,0 +1,66 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Shashlichnik
+{
+    public class ScenPart_PlayerPawnsArriveMethod : RimWorld.ScenPart_PlayerPawnsArriveMethod
+    {
+        public override void GenerateIntoMap(Map map)
+        {
+            if (Find.GameInitData == null)
+            {
+                return;
+            }
+            List<Thing> list = new List<Thing>();
+            foreach (ScenPart scenPart in Find.Scenario.AllParts)
+            {
+                list.AddRange(scenPart.PlayerStartingThings());
+            }
+            foreach (Pawn key in Find.GameInitData.startingAndOptionalPawns)
+            {
+                foreach (ThingDefCount t in Find.GameInitData.startingPossessions[key])
+                {
+                    list.Add(StartingPawnUtility.GenerateStartingPossession(t));
+                }
+            }
+
+            SpawnThings(map, MapGenerator.PlayerStartSpot, list);
+        }
+        private void SpawnThings(Map map, IntVec3 cell, List<Thing> startingItems)
+        {
+            List<List<Thing>> list = new List<List<Thing>>();
+            foreach (Pawn item in Find.GameInitData.startingAndOptionalPawns)
+            {
+                list.Add(new List<Thing>
+                {
+                    item
+                });
+            }
+            int num = 0;
+            foreach (Thing thing in startingItems)
+            {
+                if (thing.def.CanHaveFaction)
+                {
+                    thing.SetFactionDirect(Faction.OfPlayer);
+                }
+                list[num].Add(thing);
+                num++;
+                if (num >= list.Count)
+                {
+                    num = 0;
+                }
+            }
+
+            foreach(Thing thing in list.SelectMany(l => l))
+            {
+                GenPlace.TryPlaceThing(thing, cell, map, ThingPlaceMode.Radius, null, null, null, 2);
+            }
+
+        }
+    }
+}

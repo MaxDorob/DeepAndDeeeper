93f584d3a18eddb3ccbad9cf59927f3643e4a3db
Макс Дороболюк <dmv_01@mail.ru>
2026-05-02 02:35:20 +0600

    Use size from mod settings for initial map

---
+++ Source/ScenPart_ScenarioCaveMapSize.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Shashlichnik
+{
+    public class ScenPart_ScenarioCaveMapSize : ScenPart
+    {
+        private IntVec3 original;
+        public override void PreMapGenerate()
+        {
+            base.PreMapGenerate();
+            Find.GameInitData.mapSize = Mod.Settings.mapSize;
+        }
+        public override void PostGameStart()
+        {
+            base.PostGameStart();
+            Find.World.info.initialMapSize = original;
+        }
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref original, nameof(original)); //I'm not sure if this is really needed
+        }
+    }
+}

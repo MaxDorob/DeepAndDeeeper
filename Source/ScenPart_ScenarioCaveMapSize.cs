bbe2c9a4092ea67410f5e0f6b74688777480b8e3
Макс Дороболюк <dmv_01@mail.ru>
2026-05-02 03:24:16 +0600

    Page for map size

--- Source/ScenPart_ScenarioCaveMapSize.cs
+++ Source/ScenPart_ScenarioCaveMapSize.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace Shashlichnik
@@ -16,6 +17,13 @@ namespace Shashlichnik
             base.PreMapGenerate();
             Find.GameInitData.mapSize = Mod.Settings.mapSize;
         }
+        public override IEnumerable<Page> GetConfigPages()
+        {
+            if (Mod.Settings.mapSize < CaveMapSizePage.recommendedMapSize)
+            {
+                yield return new CaveMapSizePage();
+            }
+        }
         public override void PostGameStart()
         {
             base.PostGameStart();
@@ -25,6 +33,27 @@ namespace Shashlichnik
         {
             base.ExposeData();
             Scribe_Values.Look(ref original, nameof(original)); //I'm not sure if this is really needed
+        }
+        public class CaveMapSizePage : Page
+        {
+            public const int recommendedMapSize = 200;
+            public override string PageTitle => "ShashlichnikDaDCaveSizeMapWarning".Translate();
+            public override void DoWindowContents(Rect rect)
+            {
+                base.DrawPageTitle(rect);
+                Rect mainRect = base.GetMainRect(rect, 0f, false);
+                Widgets.BeginGroup(mainRect);
+                Widgets.Label(rect, "ShashlichnikDaDCaveSizeMapWarningDescription".Translate());
+                Widgets.EndGroup();
+                base.DoBottomButtons(rect, null, "ShashlichnikDadCaveSizeSetToRecommended".Translate(), new Action(this.SetToRecommended), true, true);
+            }
+
+            private void SetToRecommended()
+            {
+                Mod.Settings.mapSize = recommendedMapSize;
+                Mod.Settings.Write();
+                DoNext();
+            }
         }
     }
 }

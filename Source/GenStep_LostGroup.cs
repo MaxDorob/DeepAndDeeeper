fc087a824f94567d8edb2949e4c56a03fc47f4da
Макс Дороболюк <dmv_01@mail.ru>
2026-05-06 03:55:53 +0600

    Randomize count and PawnKindDefs in lost group

--- Source/GenStep_LostGroup.cs
+++ Source/GenStep_LostGroup.cs
@@ -25,17 +25,19 @@ namespace Shashlichnik
                     chance = 0.95f
                 }
                 ];
-            //this.subCountChances = [new CountChance() { count = 1, chance = 1f }];
+            this.availableKindDefs = [PawnKindDefOf.Colonist, DefsOf.ShashlichnikDeepDiver, PawnKindDefOf.Slave];
         }
 
-        
+        public IntRange pawnsCount = new IntRange(1, 3);
+        public List<PawnKindDef> availableKindDefs;
         protected override bool TrySpawnInterestAt(Map map, IntVec3 thingPos)
         {
             Faction faction = null;
             var pawns = new List<Pawn>();
-            for (int i = 0; i < 3; i++)
+            var count = pawnsCount.RandomInRange;
+            for (int i = 0; i < count; i++)
             {
-                var pawn = PawnGenerator.GeneratePawn(DefsOf.ShashlichnikDeepDiver, faction);
+                var pawn = PawnGenerator.GeneratePawn(availableKindDefs.RandomElement(), faction);
                 pawns.Add(pawn);
                 GenSpawn.Spawn(pawn, thingPos, map); 
             }
